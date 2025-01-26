e8a4d91cb5c34bdff6c1ccf2813e41660639c616
Sohan Deemantha <sohandeemantha@gmail.com>
2025-01-26 23:22:45 +0530

    Design main UI

--- src/main/java/browse/it/controller/MainSceneController.java
+++ src/main/java/browse/it/controller/MainSceneController.java
@@ -1,4 +1,15 @@
 package browse.it.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public TextField txtAddress;
+    public WebView wbPreview;
+
+    public void txtAddressOnAction(ActionEvent actionEvent) {
+    }
 }
