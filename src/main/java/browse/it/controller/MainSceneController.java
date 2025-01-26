67a0d42d06f8747243058ca0f2603cebccc98501
Sohan Deemantha <sohandeemantha@gmail.com>
2025-01-26 23:31:48 +0530

    Setup initialization

--- src/main/java/browse/it/controller/MainSceneController.java
+++ src/main/java/browse/it/controller/MainSceneController.java
@@ -1,5 +1,6 @@
 package browse.it.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -10,6 +11,19 @@ public class MainSceneController {
     public TextField txtAddress;
     public WebView wbPreview;
 
+    public void initialize() {
+        txtAddress.requestFocus();
+        Platform.runLater(()->txtAddress.setText("www.google.com"));
+        loadWebPage(txtAddress.getText());
+        txtAddress.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) Platform.runLater(() -> txtAddress.selectAll());
+        });
+    }
+
     public void txtAddressOnAction(ActionEvent actionEvent) {
+    }
+
+    private void loadWebPage(String url) {
+
     }
 }
